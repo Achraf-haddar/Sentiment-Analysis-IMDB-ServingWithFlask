6becd49d9db370ab0e54ee06e6a1a564342beae9
Achraf-haddar <achraf.haddar@supcom.tn>
2020-09-23 22:07:45 +0200

    first commit

---
+++ src/model.py
@@ -0,0 +1,27 @@
+import config
+import transformers
+import torch.nn as nn
+
+class BERTBaseUncased(nn.Module):
+    def __init__(self):
+        super(BERTBaseUncased, self).__init__()
+        self.bert = transformers.BertModel.from_pretrained(
+            config.BERT_PATH
+        )
+        self.bert_drop = nn.Dropout(0.3)
+        # because its a binary classification and we just need a value
+        # between 0 and 1
+        self.out = nn.Linear(768, 1)
+    
+    def forward(self, ids, mask, token_type_ids):
+        _, o2 = self.bert(
+            ids, 
+            attention_mask=mask,
+            token_type_ids=token_type_ids
+        )
+        bo = self.bert_drop(o2)
+        output = self.out(bo)
+        return output
+
+
+
